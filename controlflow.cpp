177b8cdfe75ff2e781d73f88db331d750a23b385
Naman <hypixeltrooper@gmail.com>
2026-06-27 22:13:25 +0530

    Implement age check for adult or minor
    Fix missing closing brace in if statement.

---
+++ controlflow.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age:\n";
+    cin>>age;
+    if(age>=18){
+        cout<<"You're an adult";
+    else{cout<<"You're a minor";}
+return 0;
+}
